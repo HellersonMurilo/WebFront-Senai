de54d1fe2b3fb64e5e7946bb86e98627b81dff25
HellersonMurilo <calabianquimurilo328@gmail.com>
2023-07-26 22:01:06 -0300

    teste

---
+++ script.js
@@ -0,0 +1,11 @@
+/* Criação de variaveis */
+var nome = "Murilo"
+let idade = 19
+var idadeQualquer = "19"
+const anoNascimento = 2003
+
+let listaCompras = []
+
+listaCompras.push("Maça")
+
+console.log(listaCompras);
