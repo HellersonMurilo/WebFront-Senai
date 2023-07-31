35c032c30c5a2def00b2a0d6d9c0b1e6a794e89f
HellersonMurilo <calabianquimurilo328@gmail.com>
2023-07-31 20:10:39 -0300

    implementação da calculadora

---
+++ Calculadora/calc.js
@@ -0,0 +1,27 @@
+const number = document.getElementById("number")
+const number2 = document.getElementById("number2")
+const resultado = document.getElementById("resultado")
+
+function calculo(operacao) {
+    try {
+        const finalResult = eval("Number (number.value)" + operacao + "Number (number2.value)") //eval avalia se é JS
+
+        if (isNaN(finalResult) || !isFinite(finalResult)) { //isNan e isFinite trata erros
+            alert("Entrada de dados Invaálida")
+            return
+        }
+        resultado.textContent = finalResult
+
+        if (finalResult >= 0 && finalResult <= 49 ) {
+            resultado.style.color = "red"
+        }else if (finalResult >= 50 && finalResult <= 99) {
+            resultado.style.color = "blue"
+        }else if (finalResult ) {
+            resultado.style.color= "green"
+        }
+
+    }catch (error){
+        alert (error)
+    }
+
+}
